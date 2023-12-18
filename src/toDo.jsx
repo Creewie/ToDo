a9570260cc5a4e5f34112f183ade2691b795c6e4
Creewie <tabero2006@gmail.com>
2023-12-18 20:52:21 +0100

    Data

--- src/toDo.jsx
+++ src/toDo.jsx
@@ -1,9 +1,9 @@
 import { useState } from "react";
 
 const tasks = [
-    {id:1, text:"Zadanie 1", completed:false},
-    {id:2, text:"Zadanie 2", completed:true},
-    {id:3, text:"Zadanie 3", completed:false},
+    {id:1, text:"Przykładowe Zadanie 1", completed:false},
+    {id:2, text:"Przykładowe Zadanie 2", completed:true},
+    {id:3, text:"Przykładowe Zadanie 3", completed:false},
 ]
 
 const ToDo = () =>{
@@ -25,23 +25,36 @@ const ToDo = () =>{
         }
         setToDos([...todos, newTask])
     }
-    var teraz = new Date().getTime()
+    
+        function name(params) {
+            
+        }
+        var teraz = new Date()
+        var rok = teraz.getFullYear()
+        var miesiac = teraz.getMonth()+1
+        var dzien = teraz.getDate()
+        var godzina = teraz.getHours()
+        var minuta = teraz.getMinutes()
+        var sekunda = teraz.getSeconds()
+        var chwila = (`${rok}.${miesiac}.${dzien} ${godzina}:${minuta}:${sekunda}`)
+
     return(
         
         <>
-            <h1 style={{fontSize:'35px'}}>Lista Zadań {teraz}</h1>
+            <h1 style={{fontSize:'35px'}}>Lista Zadań</h1>
+            <p>{chwila}</p>
             <ul>
                 {
                     todos.map(task => (
-                        <li style={{color:"#023047", fontSize:"14px", margin:"10px"}} 
+                        <li style={{color:"#023047", fontSize:"20px", margin:"10px"}} 
                         key={task.id}>
                             {task.text}
-                            <button style={{margin:"10px", background:"darkred"}} onClick={()=>removeTasksHandler(task.id)}>Usun zadanie {task.id}</button>
+                            <button style={{fontSize:'15px',margin:"10px", background:"darkred"}} onClick={()=>removeTasksHandler(task.id)}>Usuń</button>
                         </li>
                     ))
                 }
             </ul>
-                <button  onClick={addNewTaskHandler}>Dodaj nowe</button>
+                <button onClick={addNewTaskHandler}>Dodaj nowe</button>
         </>  
     );
 }
